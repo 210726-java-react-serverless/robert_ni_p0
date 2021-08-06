fce86f6de203abc592d12cc7d50fe72be223b686
Robert Ni <robertni@outlook.fr>
2021-08-06 10:40:18 -0400

    added implementation of ConnectionFactory and some exceptions

---
+++ src/main/java/com/revature/utils/exceptions/InvalidRequestException.java
@@ -0,0 +1,9 @@
+package com.revature.utils.exceptions;
+
+public class InvalidRequestException extends RuntimeException {
+
+    public InvalidRequestException(String msg) {
+        super(msg);
+    }
+    
+}
---
+++ src/main/java/com/revature/utils/exceptions/ResourcePersistenceException.java
@@ -0,0 +1,9 @@
+package com.revature.utils.exceptions;
+
+public class ResourcePersistenceException extends RuntimeException {
+
+    public ResourcePersistenceException(String msg) {
+        super(msg);
+    }
+
+}
